d549c1b0bdbda499c963c408cec0cb0d8359a131
EvandroMauricio <evandromauricio@id.uff.br>
2026-04-02 00:16:07 -0300

    .

---
+++ scripts.js
@@ -0,0 +1,64 @@
+// Descobri que é o botao
+let botao = document.querySelector(".botao-gerar");
+let endereco = "https://api.groq.com/openai/v1/chat/completions";
+
+// Criei a funcao que será chamada quando clicar
+// no botao
+
+async function gerarCodigo() {
+  let textoUsuario = document.querySelector(".caixa-texto").value;
+  let blocoCodigo = document.querySelector(".bloco-codigo");
+  let resultadoCodigo = document.querySelector(".resultado-codigo");
+
+  let resposta = await fetch(endereco, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Bearer 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",
+    },
+    body: JSON.stringify({
+      model: "llama-3.3-70b-versatile",
+      messages: [
+        {
+          role: "system",
+          content:
+            "Você é um gerador de código HTML e CSS. Responda SOMENTE com código puro. NUNCA use crases, markdown ou explicações. Formato: primeiro <style> com o CSS, depois o HTML. Siga EXATAMENTE o que o usuário pedir. Se pedir algo quicando, use translateY no @keyframes. Se pedir algo girando, use rotate.",
+        },
+        {
+          role: "user",
+          content: textoUsuario,
+        },
+      ],
+    }),
+  });
+
+  let dados = await resposta.json();
+  let resultado = dados.choices[0].message.content;
+
+  blocoCodigo.textContent = resultado;
+  resultadoCodigo.srcdoc = resultado;
+}
+
+// ficar de olho no botao, quando clicado chamar o gerarCodigo
+botao.addEventListener("click", gerarCodigo);
+
+// vizinho curioso (addEventListener)
+// adicionar ouvinte de eventos
+// Evento = clique, digitei...
+
+let botaoCopiar = document.querySelector(".botao-copiar");
+
+function copiarCodigo() {
+  let codigo = document.querySelector(".bloco-codigo").textContent;
+
+  navigator.clipboard.writeText(codigo);
+
+  botaoCopiar.textContent = "Copiado!";
+
+  setTimeout(() => {
+    botaoCopiar.textContent = "Copiar";
+  }, 2000);
+}
+
+botaoCopiar.addEventListener("click", copiarCodigo);
